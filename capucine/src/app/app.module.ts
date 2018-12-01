3fdd286c5b71a6c76700bfebee49cb2f6812bdac
Capucine Leclerc <capucine_49@hotmail.com>
2018-12-01 12:51:49 +0100

    create components

--- capucine/src/app/app.module.ts
+++ capucine/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderMenuComponent } from './menu/header-menu/header-menu.component';
+import { LinksMenuComponent } from './menu/links-menu/links-menu.component';
+import { LinksNetworksMenuComponent } from './menu/links-networks-menu/links-networks-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    HeaderMenuComponent,
+    LinksMenuComponent,
+    LinksNetworksMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ capucine/src/app/menu/links-menu/links-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-links-menu',
+  templateUrl: './links-menu.component.html',
+  styleUrls: ['./links-menu.component.scss']
+})
+export class LinksMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
