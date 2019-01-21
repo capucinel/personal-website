83c35879e53f478482e7c22203226689ef99d976
Capucine <capucine_49@hotmail.com>
2019-01-21 17:55:51 +0100

    router

--- capucine/src/app/app.module.ts
+++ capucine/src/app/app.module.ts
@@ -6,20 +6,38 @@ import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TypingAnimationDirective } from 'angular-typing-animation';
+import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuComponent,
+    AboutComponent,
     TypingAnimationDirective
   ],
   imports: [
     BrowserModule,
-    FlexLayoutModule
-
-
+    FlexLayoutModule,
+    RouterModule.forRoot(
+        appRoutes,
+        { enableTracing: true } // <-- debugging purposes only
+      )
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
