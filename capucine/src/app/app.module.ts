a312e8e5a9ded071cebf7bf73ddabd3b32e60fd1
Capucine <capucine_49@hotmail.com>
2019-01-22 17:56:16 +0100

    test routes

--- capucine/src/app/app.module.ts
+++ capucine/src/app/app.module.ts
@@ -3,22 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MenuComponent } from './menu/menu.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TypingAnimationDirective } from 'angular-typing-animation';
 import { RouterModule, Routes } from '@angular/router';
+import {MenuComponent} from './menu/menu.component';
+import {ContactComponent} from './contact/contact.component';
+import {ResumeComponent} from './resume/resume.component';
 import {AboutComponent} from './about/about.component';
 
 const appRoutes: Routes = [
-  {
-    path: 'about',
+
+  { path: '',
+    component: HomeComponent
+  },
+
+  { path: 'about',
     component: AboutComponent
   },
-  { path: '',
-    redirectTo: '/',
-    pathMatch: 'full'
-  },
-  // { path: '**', component: PageNotFoundComponent }
+
 ];
 
 @NgModule({
@@ -26,8 +28,10 @@ const appRoutes: Routes = [
     AppComponent,
     HomeComponent,
     MenuComponent,
+    ContactComponent,
+    ResumeComponent,
     AboutComponent,
-    TypingAnimationDirective
+    TypingAnimationDirective,
   ],
   imports: [
     BrowserModule,
