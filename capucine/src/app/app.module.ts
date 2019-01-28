cc86ed1dd4320f1419c3d9bd4077720620631165
Capucine Leclerc <capucine_49@hotmail.com>
2019-01-28 20:08:47 +0100

    prepare routing

--- capucine/src/app/app.module.ts
+++ capucine/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
@@ -45,7 +45,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
         appRoutes,
         { enableTracing: true } // <-- debugging purposes only
-      )
+      ),
+      BrowserAnimationsModule
   ],
   exports: [RouterModule],
   providers: [],
